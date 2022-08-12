bcf4460d4a343584d80233cc6bf847a516f70e31
pa <1020401660@qq.com>
2022-08-13 03:09:07 +0800

    写点注释

--- ltt_server/src/db/user.rs
+++ ltt_server/src/db/user.rs
@@ -14,6 +14,8 @@ pub struct UserDbHandler{
     _client:Vec<Client>
 }
 impl UserDbHandler{
+    //对应user 表的dao层，持有到数据库的消息通道，
+    //后续数据库断开没有关系，在数据库重新链接后会来更新这里的client
     pub fn new() -> UserDbHandler {
         UserDbHandler{
             _client:Vec::new()
@@ -26,15 +28,18 @@ impl UserDbHandler{
             std::mem::swap(&mut client, &mut self._client[0])
         }
     }
+
+    //此处的增删改查由service层的usermanager调用，处理api请求
+    // 此时可以保证client存在，因为数据库在结束启动时，
+    // 会等待usermanager加载完内存，而加载内存就来自一次数据库读取
     pub async fn db_get_all_user(&self)->Option<Vec<Row>>{
-        if !self._client.is_empty(){
             let res=self._client[0]
                 .query("select * from user_info",&[])
                 .await;
             if let Ok(res_)=res{
                 return Some(res_)
             }
-        }
+
         return None;
     }
     pub async fn db_create_user(&self,new_user : &User) -> Option<Vec<Row>> {
@@ -43,10 +48,6 @@ impl UserDbHandler{
         插入错误处理，原则上不会出错，毕竟接口处做了检查
         传递过来user各项数据必须满足数据库要求
          */
-        if self._client.is_empty(){
-            return None;
-        }
-
         let mut insert_cmd = format!("insert into user_info values ({},'{}','{}','{}')"
                                      ,new_user.id,new_user.username,new_user.password,new_user.email);
         println!("insert_cmt {}",insert_cmd);
