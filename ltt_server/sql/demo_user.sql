51760d4ef7a8507e125ef9df11ca03eb49a96ee5
pa <1020401660@qq.com>
2022-08-24 13:42:21 +0800

    demo扔了，和代码里的文本没法同步，不如不要

--- ltt_server/sql/demo_user.sql
+++
@@ -1,8 +0,0 @@
-
------------------------------------------------------------------增
-insert into user_info(username, password, email)
-values ('haskd', 'sadasdasd', 'sdasdsd')
-RETURNING id;
-
------------------------------------------------------------------查
-select * from user_info;
