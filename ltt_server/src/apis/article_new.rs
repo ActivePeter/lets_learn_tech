5491f916dc64e455184da6bd5c89fe7a327819f6
pa <1020401660@qq.com>
2022-08-28 01:39:19 +0800

    发布文章 消息

--- ltt_server/src/apis/article_new.rs
+++ ltt_server/src/apis/article_new.rs
@@ -3,13 +3,15 @@ use axum::response::IntoResponse;
 use axum::http::StatusCode;
 use serde::{Deserialize, Serialize};
 use crate::services::token;
-use crate::services::user_manager::G_USER_MANAGER;
+use crate::services::user_manager::{G_USER_MANAGER, UserManager};
 use crate::models::user::{User, UserId};
 use std::future::Future;
 use crate::services::token::CheckTokenRes;
 use crate::models::tag::TagId;
 use crate::models::article::{Article, ArticleId};
 use crate::services;
+use crate::models::path::article_path;
+
 pub async fn article_new(
     Json(payload): Json<ArticleNewRequest>,
 ) -> impl IntoResponse {
@@ -20,6 +22,13 @@ pub async fn article_new(
                 payload.uid,payload.tags,payload.content,payload.rawtext,payload.title
             ).await;
             if let Some(res)=res{
+                services::robot_service::G_ROBOT_MAN.send_msg(
+                    &format!("{} 在社区发布了文章 《{}》, 快去看看吧！{}",
+                             UserManager::get().search_user_by_id(payload.uid).await.unwrap().username,
+                             payload.title.trim_end(),
+                             article_path(res)
+                    )
+                ).await;
                 return (StatusCode::OK, serde_json::to_string(&ArticleNewResponse{
                     articleid: res
                 }).unwrap()).into_response();
